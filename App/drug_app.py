b1e988f6dd1e07357b52a5a3e73a04239cff0423
Alexandre Langlois <langlois.hand@gmail.com>
2025-06-02 16:56:56 +0200

    Test Modification Main
    Modification mineur sur Main pour voir si la CI passe entièrement (une différence entre les 2 branches main et update donc elle devrait mettre à jour update).

--- App/drug_app.py
+++ App/drug_app.py
@@ -49,7 +49,7 @@ examples = [
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
-article = "TEST MODIFICATION - This app is a part of the Beginner's Guide to CI/CD for Machine Learning. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
+article = "TEST MODIFICATION 2 - This app is a part of the Beginner's Guide to CI/CD for Machine Learning. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
 
 
 gr.Interface(
